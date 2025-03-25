d7d2011e3916ce4d494bdeadf0c1de8a504340ab
Dilan kanishka <dilankanishka032@gmail.com>
2025-03-25 16:44:09 +0530

    develop all user end-point (CRUD)

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -23,12 +23,17 @@ const createUser = async(req, res) => {
     if(!name){ return res.status(400).json({ message: 'Name is required' }); } // check if name is empty
     if(!email){ return res.status(400).json({ message: 'Email is required' }); } // check if email is empty
     if(!age){ return res.status(400).json({ message: 'Age is required' }); } // check if age is empty    
+
+    // check if the user email already exists
+    const users = await user.find();
+    const emailExists = users.find(u => u.email === email);
+    if(emailExists){ return res.status(400).json({ message: 'Email already exists' }); } 
     
     const newUser = new user({ name, email, age }); // create a new user
 
     try {
         await newUser.save(); // save the new user
-        res.status(201).json(newUser);
+        res.status(201).json({message: 'User created successfully', newUser}); // display a success message
 
     } catch (error) {
         res.status(409).json({ message: error.message });
@@ -36,5 +41,81 @@ const createUser = async(req, res) => {
 
 } // end of create User
 
+
+// get user by id
+const getUserById = async (req, res) => {
+    const { id } = req.params; // get the id from the request parameters
+
+    if (!id) { // check if id is empty
+        return res.status(400).json({ message: 'Id is required' });
+    }
+
+    try {
+        const userById = await user.findById(id); // find the user by id
+
+        if (!userById) { // check user id availability
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return res.status(200).json(userById); // display the user by id
+    
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+} // end of getUserById
+
+
+// update user by id
+const updateUserById = async (req, res) => {
+    const { id } = req.params; // get the id from the request parameters
+    const { name, email, age } = req.body; // get the name, email, and age from the request body
+
+    if (!id) { // check if id is empty
+        return res.status(400).json({ message: 'Id is required' });
+    }
+
+    try {
+        // Find the user by id and update the fields
+        const userById = await user.findByIdAndUpdate(id, { name, email, age }, 
+        { new: true } // this returns the updated user document
+        );
+
+        if (!userById) { // check if the user was found and updated
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json({ message: 'User updated successfully', updateuserID: userById }); // return success message and updated user
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+} // end of updateUserById
+
+
+// delete user by id
+const deleteUserById = async (req, res) => {
+    const { id } = req.params; // get the id from the request parameters
+
+    if (!id) { // check if id is empty
+        return res.status(400).json({ message: 'Id is required' });
+    }
+
+    try {
+        // Find the user by id and delete
+        const userById = await user.findByIdAndDelete(id);
+
+        if (!userById) { // check if the user was found and deleted
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json({ message: 'User deleted successfully', deletedUserID: userById }); // return success message and deleted user
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+} // end of deleteUserById
+
 exports.getAllUsers = getAllUsers;
 exports.createUser = createUser;
+exports.getUserById = getUserById;
+exports.updateUserById = updateUserById;
+exports.deleteUserById = deleteUserById;
--- Route/UserRoute.js
+++ Route/UserRoute.js
@@ -5,5 +5,8 @@ const userController = require('../Controllers/UserController');
 // Define the '/users' route
 router.get('/', userController.getAllUsers);
 router.post('/create', userController.createUser);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUserById);
+router.delete('/:id', userController.deleteUserById);
 
 module.exports = router;

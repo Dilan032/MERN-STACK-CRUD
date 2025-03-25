50a323c0473e9168aa6e94f6abc6234de0ada585
Dilan kanishka <dilankanishka032@gmail.com>
2025-03-25 07:43:31 +0530

    develop mongooes DB connection

---
+++ app.js
@@ -0,0 +1,28 @@
+// DB password : mWFiDJtYQE4AXQ5Z
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://user1:mWFiDJtYQE4AXQ5Z@cruddb.c18zv.mongodb.net/?appName=crudDB";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
